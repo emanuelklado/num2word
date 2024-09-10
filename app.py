92ff6fba1754f307f0ee42162499f74ab104be20
Emanuel <emanuelklado@msn.com>
2024-09-10 14:19:05 -0300

    subir app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from num2words import num2words
+
+app = Flask(__name__)
+
+@app.route('/numero_por_extenso', methods=['GET'])
+def numero_por_extenso():
+    numero = request.args.get('numero', default=None, type=int)
+    if numero is None:
+        return jsonify({'erro': 'Número não fornecido'}), 400
+    
+    numero_extenso = num2words(numero, lang='pt_BR')
+    return jsonify({'numero': numero, 'por_extenso': numero_extenso})
+
+if __name__ == '__main__':
+    app.run(debug=True)
